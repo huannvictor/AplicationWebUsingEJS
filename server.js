1fffff5623d2e1b271c0e56874c7d8477cc9e54b
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-10-20 15:13:28 -0300

    using forEach

--- server.js
+++ server.js
@@ -2,20 +2,48 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
-
+/*
 app.get('/', (req, res) => res.render('pages/index'))
-/*
-app.get("/", function(req, res){
-  res.render("index");
-})
 */
+app.get('/', function (req, res) {
+  const items = [
+    {
+      title: 'D',
+      message: 'esenvolver aplicações/serviços de forma fácil'
+    },
+    {
+      title: 'M',
+      message: 'aravigold'
+    },
+    {
+      title: 'A',
+      message: 'rraza com tudoooo'
+    },
+    {
+      title: 'I',
+      message: 'nigualável'
+    },
+    {
+      title: 'S',
+      message: 'uper Diva'
+    }
+  ]
 
-app.get('/sobre', (req, res) => res.render('pages/about'))
-/*
-app.get("/sobre", function(req, res){
-  res.render("about");
+  const subtitle =
+    'Uma linguagem de modelagem para criação de páginas HTML ultilizndo Javascript'
+
+  res.render('pages/index', {
+    qualities: items,
+    subtitle: subtitle
+  })
 })
+
+/*
+app.get('/sobre', (req, res) => res.render('pages/about'))
 */
+app.get('/sobre', function (req, res) {
+  res.render('pages/about')
+})
 
 app.listen(8080)
 console.log('rodando')
